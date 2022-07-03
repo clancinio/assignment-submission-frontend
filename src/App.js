7457945402c5117d0e35837cfa08f1f60cae2773
Clancinio <dean.clancy91@gmail.com>
2022-07-03 19:02:14 +0100

    Add fetch call to auth endpoint

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+const requestBody = {
+  username: "jon",
+  password: "asdfasdf",
+};
+
+fetch("http://localhost:8081/auth", {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+  },
+  body: JSON.stringify(requestBody),
+})
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data);
+  });
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Hello</h1>
       </header>
     </div>
   );
