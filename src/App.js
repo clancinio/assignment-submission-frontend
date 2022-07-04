38f341273be31399815b5fb63fdaa1a94e1c2de9
Clancinio <dean.clancy91@gmail.com>
2022-07-04 11:52:58 +0100

    Store jwt in local storage

--- src/App.js
+++ src/App.js
@@ -1,27 +1,38 @@
 import "./App.css";
-
-const requestBody = {
-  username: "jon",
-  password: "asdfasdf",
-};
-
-fetch("http://localhost:8081/auth", {
-  method: "POST",
-  headers: {
-    "Content-Type": "application/json",
-  },
-  body: JSON.stringify(requestBody),
-})
-  .then((response) => response.json())
-  .then((data) => {
-    console.log(data);
-  });
+import { useEffect } from "react";
+import useLocalState from "./utils/useLocalState";
 
 function App() {
+  // Use custom useState hook to store jwt in local storage
+  const [jwt, setJwt] = useLocalState("", "jwt");
+
+  useEffect(() => {
+    // If there is no jwt set, then go fetch one
+    if (!jwt) {
+      const requestBody = {
+        username: "jon",
+        password: "asdfasdf",
+      };
+
+      fetch("http://localhost:8081/auth", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(requestBody),
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          setJwt(data.jwtToken);
+        });
+    }
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Hello</h1>
+        <p>JWT is: {jwt}</p>
       </header>
     </div>
   );
