a1c8ddf2bad363146c488da58bacc13067efbdaa
Clancinio <dean.clancy91@gmail.com>
2022-07-04 11:53:21 +0100

    Custom useStage hook

---
+++ src/utils/useLocalState.js
@@ -0,0 +1,16 @@
+import { useState, useEffect } from "react";
+
+function useLocalState(defaultValue, key) {
+  const [value, setValue] = useState(() => {
+    const localStorageValue = window.localStorage.getItem(key);
+    return localStorageValue !== null
+      ? JSON.parse(localStorageValue)
+      : defaultValue;
+  });
+  useEffect(() => {
+    window.localStorage.setItem(key, JSON.stringify(value));
+  }, [key, value]);
+  return [value, setValue];
+}
+
+export default useLocalState;
