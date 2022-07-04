bdd4b12bd9446b9f8242fe1fbebcd30fc7722144
Clancinio <dean.clancy91@gmail.com>
2022-07-04 12:58:16 +0100

    Make Dashboard Compenet a private route

--- src/App.js
+++ src/App.js
@@ -5,35 +5,45 @@ import { useLocalState } from "./utils/useLocalState";
 import { Routes, Route } from "react-router-dom";
 import Dashboard from "./components/dashboard/Dashboard";
 import HomePage from "./components/home/HomePage";
+import Login from "./components/login/Login";
+import PrivateRoute from "./components/privateroute/PrivateRoute";
 
 function App() {
   const [jwt, setJwt] = useLocalState("", "jwt");
 
-  useEffect(() => {
-    // If there is no jwt set, then go fetch one
-    if (!jwt) {
-      const requestBody = {
-        username: "jon",
-        password: "asdfasdf",
-      };
+  // useEffect(() => {
+  //   // If there is no jwt set, then go fetch one
+  //   if (!jwt) {
+  //     const requestBody = {
+  //       username: "jon",
+  //       password: "asdfasdf",
+  //     };
 
-      fetch("http://localhost:8081/auth", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(requestBody),
-      })
-        .then((response) => response.json())
-        .then((data) => {
-          setJwt(data.jwtToken);
-        });
-    }
-  }, []);
+  //     fetch("http://localhost:8081/auth", {
+  //       method: "POST",
+  //       headers: {
+  //         "Content-Type": "application/json",
+  //       },
+  //       body: JSON.stringify(requestBody),
+  //     })
+  //       .then((response) => response.json())
+  //       .then((data) => {
+  //         setJwt(data.jwtToken);
+  //       });
+  //   }
+  // }, []);
 
   return (
     <Routes>
-      <Route path="/dashboard" element={<Dashboard />} />
+      <Route
+        path="/dashboard"
+        element={
+          <PrivateRoute>
+            <Dashboard />
+          </PrivateRoute>
+        }
+      />
+      <Route path="/login" element={<Login />} />
       <Route path="/" element={<HomePage />} />
     </Routes>
   );
