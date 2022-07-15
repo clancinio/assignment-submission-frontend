9b257035cfaaf4a110ce6870c991c993daa220f0
Clancinio <dean.clancy91@gmail.com>
2022-07-15 15:15:40 +0100

    Creating a new Assignment with CRUD Part III

--- src/components/assignmentview/AssignmentView.js
+++ src/components/assignmentview/AssignmentView.js
@@ -6,7 +6,10 @@ function AssignmentView() {
   // Use custom useState hook to store jwt in local storage
   const [jwt, setJwt] = useLocalState("", "jwt");
   // store assignments
-  const [assignment, setAssignment] = useState();
+  const [assignment, setAssignment] = useState({
+    gitHubUrl: "",
+    branch: "",
+  });
   // Get ASsignment id from url param
   const { assignmentId } = useParams();
 
@@ -29,6 +32,36 @@ function AssignmentView() {
       });
   }, []);
 
+  const updateAssignment = (prop, value) => {
+    const newAssignment = { ...assignment };
+    newAssignment[prop] = value;
+    setAssignment(newAssignment);
+    console.log(assignment);
+  };
+
+  const save = () => {
+    fetch(`http://localhost:8081/api/assignments/${assignmentId}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + jwt,
+      },
+      body: JSON.stringify(assignment),
+    })
+      .then((response) => {
+        if (response.status === 200) {
+          return response.json();
+        }
+      })
+      .then((assignment) => {
+        console.log(assignment);
+        setAssignment(assignment);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
     <div>
       <h1>AssignmentView</h1>
@@ -36,11 +69,26 @@ function AssignmentView() {
         <>
           <h2>Status: {assignment.status}</h2>
           <h2>
-            GitHub URL: <input type="url"></input>
+            GitHub URL:{" "}
+            <input
+              type="url"
+              value={assignment.gitHubUrl}
+              onChange={(e) => {
+                updateAssignment("gitHubUrl", e.target.value);
+              }}
+            ></input>
           </h2>
           <h2>
-            Branch: <input type="text"></input>
+            Branch:{" "}
+            <input
+              type="text"
+              value={assignment.branch}
+              onChange={(e) => {
+                updateAssignment("branch", e.target.value);
+              }}
+            ></input>
           </h2>
+          <button onClick={() => save()}>Submit Assignment</button>
         </>
       ) : (
         <></>
