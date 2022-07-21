1e807847716d6fd85b5d884f6de8b8c5550a4dd1
Clancinio <dean.clancy91@gmail.com>
2022-07-21 12:57:10 +0100

    Add card layout for assignments

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,9 +1,16 @@
 import React, { useState, useEffect } from "react";
 import { useLocalState } from "../../utils/useLocalState";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import Container from "react-bootstrap/Container";
+import Card from "react-bootstrap/Card";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import Button from "react-bootstrap/Button";
 import httpRequest from "../../services/httpRequestService";
 
 function Dashboard() {
+  const navigate = useNavigate();
+
   // Use custom useState hook to store jwt in local storage
   const [jwt] = useLocalState("", "jwt");
 
@@ -28,24 +35,46 @@ function Dashboard() {
   };
 
   return (
-    <div style={{ margin: "2em" }}>
+    <Container>
+      <Button className="mt-5" size="large" onClick={() => createAssignment()}>
+        Create New Assignment
+      </Button>
       {assignments ? (
-        assignments.map((assignment) => {
-          return (
-            <div key={assignment.id}>
-              <Link to={`/assignments/${assignment.id}`}>
-                Assignment: {assignment.id}
-              </Link>
-            </div>
-          );
-        })
+        <Row className="d-flex flex-row gap-5 justify-content-center my-5">
+          {assignments.map((assignment) => {
+            return (
+              <Col lg={4} xl={3} key={assignment.id}>
+                <Card style={{ height: "18rem" }} className="shadow rounded">
+                  <Card.Body className="d-flex flex-column">
+                    <Card.Title>Assignment {assignment.id}</Card.Title>
+                    <Card.Subtitle className="mb-2 text-muted">
+                      {assignment.status}
+                    </Card.Subtitle>
+                    <Card.Text>
+                      <p>GitHubURL: {assignment.gitHubUrl}</p>
+                      <p>Branch: {assignment.branch}</p>
+                    </Card.Text>
+                    <Button
+                      variant="secondary"
+                      className="mt-auto p-2"
+                      onClick={() => {
+                        navigate(`/assignments/${assignment.id}`);
+                      }}
+                    >
+                      Edit
+                    </Button>
+                  </Card.Body>
+                </Card>
+              </Col>
+            );
+          })}
+        </Row>
       ) : (
         <div>
-          <h3>No assignemnt available</h3>
+          <h3>No assignemnts available</h3>
         </div>
       )}
-      <button onClick={() => createAssignment()}>Submit Assignment</button>
-    </div>
+    </Container>
   );
 }
 
