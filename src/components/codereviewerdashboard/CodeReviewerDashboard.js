93fd91ac84a1bfcf49e95cde3727769abf700712
Clancinio <dean.clancy91@gmail.com>
2022-07-25 16:12:54 +0100

    Logic for Claiming Assignments

--- src/components/codereviewerdashboard/CodeReviewerDashboard.js
+++ src/components/codereviewerdashboard/CodeReviewerDashboard.js
@@ -8,6 +8,7 @@ import Row from "react-bootstrap/Row";
 import Button from "react-bootstrap/Button";
 import Badge from "react-bootstrap/Badge";
 import NavBar from "../navbar/NavBar";
+import httpRequest from "../../services/httpRequestService";
 
 function CodeReviewerDashboard() {
   const navigate = useNavigate();
@@ -16,7 +17,34 @@ function CodeReviewerDashboard() {
   const [jwt] = useLocalState("", "jwt");
 
   // store assignments
+  const [assignmentsInReview, setAssignmentsInReview] = useState([]);
   const [assignments, setAssignments] = useState([]);
+  const [assignmentsNeedUpdate, setAssignmentsNeedUpdate] = useState([]);
+
+  function claimAssignment(assignment) {
+    const requestBody = { ...assignment };
+    httpRequest(
+      `http://localhost:8081/api/assignments/claim`,
+      "PUT",
+      jwt,
+      requestBody
+    ).then((updatedAssignment) => {
+      console.log(updatedAssignment);
+      // Update date the view for the assignment that changed
+      const assignmentsCopy = [...assignments];
+      const i = assignmentsCopy.findIndex((a) => a.id === assignment.id);
+      assignmentsCopy[i] = updatedAssignment;
+      setAssignments(assignmentsCopy);
+    });
+  }
+
+  useEffect(() => {
+    httpRequest("http://localhost:8081/api/assignments", "GET", jwt).then(
+      (assignments) => {
+        setAssignments(assignments);
+      }
+    );
+  }, []);
 
   return (
     <>
@@ -27,50 +55,158 @@ function CodeReviewerDashboard() {
             <h1 className="my-3">Code Reviewer Dashboard</h1>
           </Col>
         </Row>
-        {assignments.length > 0 ? (
-          <Row className="d-flex flex-row gap-5 justify-content-center my-5">
-            {assignments.map((assignment) => {
-              return (
-                <Col lg={4} xl={3} key={assignment.id}>
-                  <Card style={{ height: "18rem" }} className="shadow rounded">
-                    <Card.Body className="d-flex flex-column">
-                      <Card.Title>Assignment {assignment.number}</Card.Title>
-                      <div className="d-flex align-items-start">
-                        <Badge
-                          pill
-                          className="mb-2"
-                          style={{ fontSize: "1em" }}
-                        >
-                          {assignment.status}
-                        </Badge>
-                      </div>
+        <div className="assignment--section in-review">
+          <h3>Assignments in Review</h3>
+          {assignmentsInReview.length > 0 ? (
+            <Row className="d-flex flex-row gap-5 justify-content-center my-5">
+              {assignmentsInReview.map((assignment) => {
+                return (
+                  <Col lg={4} xl={3} key={assignment.id}>
+                    <Card
+                      style={{ height: "18rem" }}
+                      className="shadow rounded"
+                    >
+                      <Card.Body className="d-flex flex-column">
+                        <Card.Title>Assignment {assignment.number}</Card.Title>
+                        <div className="d-flex align-items-start">
+                          <Badge
+                            pill
+                            className="mb-2"
+                            style={{ fontSize: "1em" }}
+                          >
+                            {assignment.status}
+                          </Badge>
+                        </div>
 
-                      <Card.Text>
-                        <p>GitHubURL: {assignment.gitHubUrl}</p>
-                        <p>Branch: {assignment.branch}</p>
-                      </Card.Text>
-                      <Button
-                        variant="secondary"
-                        className="mt-auto p-2"
-                        onClick={() => {
-                          navigate(`/assignments/${assignment.id}`);
-                        }}
-                      >
-                        Edit
-                      </Button>
-                    </Card.Body>
-                  </Card>
-                </Col>
-              );
-            })}
-          </Row>
-        ) : (
-          <Row>
-            <Col className="my-3">
-              <h3>There are no assignments available for review</h3>
-            </Col>
-          </Row>
-        )}
+                        <Card.Text>
+                          <p>GitHubURL: {assignment.gitHubUrl}</p>
+                          <p>Branch: {assignment.branch}</p>
+                        </Card.Text>
+                        <Button
+                          variant="secondary"
+                          className="mt-auto p-2"
+                          onClick={() => {}}
+                        >
+                          Claim
+                        </Button>
+                      </Card.Body>
+                    </Card>
+                  </Col>
+                );
+              })}
+            </Row>
+          ) : (
+            <Row>
+              <Col className="my-3">
+                <h5 className="text-muted">
+                  There are no assignments available for review
+                </h5>
+              </Col>
+            </Row>
+          )}
+        </div>
+        <div className="assignment--section submitted">
+          <h3>Submitted Assignments</h3>
+          {assignments.length > 0 ? (
+            <Row className="d-flex flex-row gap-5 justify-content-center my-5">
+              {assignments.map((assignment) => {
+                return (
+                  <Col lg={4} xl={3} key={assignment.id}>
+                    <Card
+                      style={{ height: "18rem" }}
+                      className="shadow rounded"
+                    >
+                      <Card.Body className="d-flex flex-column">
+                        <Card.Title>Assignment {assignment.number}</Card.Title>
+                        <div className="d-flex align-items-start">
+                          <Badge
+                            pill
+                            className="mb-2"
+                            style={{ fontSize: "1em" }}
+                          >
+                            {assignment.status}
+                          </Badge>
+                        </div>
+
+                        <Card.Text>
+                          <p>GitHubURL: {assignment.gitHubUrl}</p>
+                          <p>Branch: {assignment.branch}</p>
+                        </Card.Text>
+                        <Button
+                          variant="secondary"
+                          className="mt-auto p-2"
+                          onClick={() => {
+                            claimAssignment(assignment);
+                          }}
+                        >
+                          Claim
+                        </Button>
+                      </Card.Body>
+                    </Card>
+                  </Col>
+                );
+              })}
+            </Row>
+          ) : (
+            <Row>
+              <Col className="my-3">
+                <h3 className="text-muted">
+                  You are not reviewing any assignments
+                </h3>
+              </Col>
+            </Row>
+          )}
+        </div>
+        <div className="assignment--section needs-update">
+          <h3>Needs update</h3>
+          {assignmentsNeedUpdate.length > 0 ? (
+            <Row className="d-flex flex-row gap-5 justify-content-center my-5">
+              {assignmentsNeedUpdate.map((assignment) => {
+                return (
+                  <Col lg={4} xl={3} key={assignment.id}>
+                    <Card
+                      style={{ height: "18rem" }}
+                      className="shadow rounded"
+                    >
+                      <Card.Body className="d-flex flex-column">
+                        <Card.Title>Assignment {assignment.number}</Card.Title>
+                        <div className="d-flex align-items-start">
+                          <Badge
+                            pill
+                            className="mb-2"
+                            style={{ fontSize: "1em" }}
+                          >
+                            {assignment.status}
+                          </Badge>
+                        </div>
+
+                        <Card.Text>
+                          <p>GitHubURL: {assignment.gitHubUrl}</p>
+                          <p>Branch: {assignment.branch}</p>
+                        </Card.Text>
+                        <Button
+                          variant="secondary"
+                          className="mt-auto p-2"
+                          onClick={() => {
+                            claimAssignment();
+                          }}
+                        >
+                          Claim
+                        </Button>
+                      </Card.Body>
+                    </Card>
+                  </Col>
+                );
+              })}
+            </Row>
+          ) : (
+            <Row>
+              <Col className="my-3">
+                <h5 className="text-muted">No assignments for updateing</h5>
+              </Col>
+            </Row>
+          )}
+        </div>
       </Container>
     </>
   );
